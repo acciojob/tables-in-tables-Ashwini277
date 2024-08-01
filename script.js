295128fa58b51e83cc064c509f46e9b294c8bf20
Ashwini277 <devarushiashwini@gmail.com>
2024-08-01 20:53:57 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,23 @@
+describe('Table Test', () => {
+    it('should verify nested tables and content', () => {
+        cy.get('table').should('have.length', 5);
+
+        cy.get('table').each(($table, index) => {
+            if (index === 1) {
+                cy.wrap($table).find('th').eq(0).should('contain', 'Nested Table 2');
+                cy.wrap($table).find('th').eq(1).should('contain', 'Column 1');
+                cy.wrap($table).find('th').eq(2).should('contain', 'Column 2');
+            }
+
+            if (index === 3) {
+                cy.wrap($table).find('th').eq(0).should('contain', 'Nested Table 3');
+                cy.wrap($table).find('td').eq(0).should('contain', 'Nested Table');
+            }
+
+            if (index === 4) {
+                cy.wrap($table).find('th').eq(0).should('contain', 'Nested Table 4');
+                cy.wrap($table).find('td').contains('HTML').should('have.css', 'background-color', 'rgb(0, 0, 0)');
+            }
+        });
+    });
+});
